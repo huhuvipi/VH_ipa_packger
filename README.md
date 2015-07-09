dde422fb716677051194b7f61b69bbce0cf724bc
Vinh Huynh <vinh.huynh@soldenservices.com>
2015-07-09 14:17:54 +0700

    creat and first commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # VH_ipa_packger
-Invalid Swift Support - The bundle contains an invalid implementation of Swift. The app may have been built or signed with non-compliant or pre-release tools.
+This script packages adding SwiftSupport folder with libswiftCore*.dylib in to ipa file. This is needed to deploy an app made with Swift into the AppStore
+- Usage : 
+--Run the script:
+ path/to/package_ipa.sh /path/to/ipafile
+--Requirements
+OSX Mavericks or Yosemite
+Xcode 6
+Xcode command line tools
---
+++ package_ipa.sh
@@ -0,0 +1,32 @@
+IPA="$1"
+TEMP_IPA_BUILT="/tmp/ipabuild"
+DEVELOPER_DIR=`xcode-select --print-path`
+APP="${TEMP_IPA_BUILT}/Payload/*.app"
+if [ ! -d "${DEVELOPER_DIR}" ]; then
+    echo "No developer directory found!"
+    exit 1
+fi
+#reparing for temp directory
+if [ -d "${TEMP_IPA_BUILT}" ];
+then
+    rm -rf "${TEMP_IPA_BUILT}"
+fi  
+
+mkdir ${TEMP_IPA_BUILT}
+echo "unzip the ipa"
+unzip -q "${IPA}.ipa" -d "${TEMP_IPA_BUILT}"
+
+echo "+ Adding SWIFT support (if necessary)"
+mkdir -p "${TEMP_IPA_BUILT}/SwiftSupport"
+
+for SWIFT_LIB in $(ls -1 ${APP}/Frameworks); do 
+    echo "Copying ${SWIFT_LIB}"
+    cp "${DEVELOPER_DIR}/Toolchains/XcodeDefault.xctoolchain/usr/lib/swift/iphoneos/${SWIFT_LIB}" "${TEMP_IPA_BUILT}/SwiftSupport"
+done
+
+rm -rf "${IPA}"
+
+echo "+ zip --symlinks --verbose --recurse-paths ${IPA} ."
+cd "${TEMP_IPA_BUILT}"
+zip --symlinks --verbose --recurse-paths "${IPA}.ipa" 
+echo "===ADDING SWIFT_LIB SUCCESS===" .
